91badf1c9001774e1cba6c3f12fde4c5190d9c98
BuildTools <unconfigured@null.spigotmc.org>
2022-10-22 16:19:33 -0700

    Completed walking and idling animations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame, sys
 from settings import *
-
+from level import Level
 
 
 class Game:
@@ -9,6 +9,7 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption('Starboo Valley')
         self.clock = pygame.time.Clock()
+        self.level = Level()
 
     def run(self):
         while True:
@@ -18,6 +19,7 @@ class Game:
                     sys.exit()
 
             dt = self.clock.tick() / 1000
+            self.level.run(dt)
             pygame.display.update()
 
 
